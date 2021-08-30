da303bf640d38de2ba1ba5b6191f2f44f9c9d20a
Alejandro Ibarra <coodingalex@gmail.com>
2021-08-29 18:15:29 -0600

    lexer: illegar chars, assign, plus, eof

--- lpp/token.py
+++ lpp/token.py
@@ -17,11 +17,10 @@ class TokenType(Enum):
     INT = auto()
     LBRACE = auto()
     LET = auto()
-    LPARENT = auto()
-    LPARENT = auto()
+    LPAREN = auto()
     PLUS = auto()
     RBRACE = auto()
-    RPARENT = auto()
+    RPAREN = auto()
     SEMICOLON = auto()
 
 class Token(NamedTuple):
